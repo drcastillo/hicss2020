7cfb460c35ced970577ef505f432738479453344
jdineen123 <jdineen81294@gmail.com>
2019-05-09 12:07:23 -0700

    consolidated utils

--- helpful_util.py
+++ helpful_util.py
@@ -11,7 +11,8 @@ import warnings
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import sys
+import glob
 
 import pickle
 
@@ -197,4 +198,45 @@ def plot_confusion_matrix(cm, classes,
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
+	
+def list_dir(verbose = True):
+    '''
+    function to list the contents of current working directory
+    return:: list of branches
+	     dict of current working directory {idx : branch}
+    '''
+    idx = []
+    contents = []
+    cwd = os.getcwd()
+    tree = os.listdir(cwd)
+    for i,j in enumerate(tree):
+        idx.append(i)
+        contents.append(j)
+    if verbose:
+        print("Working Dir: {}".format(cwd))
+        print("Returning Contents of Working Directory..")
+    return contents, dict(zip(idx, contents))
+
+def fetch_data_path (folder = 'data'):
+    '''
+    function to string concat holistic path to data files w/ user input
+    arguments:
+        pass in string of data folder
+    return :: string of concatenated path to data file
+    
+    '''
+    cwd = os.getcwd()
+    path = cwd + "\\" + folder
+    print("Choose a file from data directory:")
+    for idx, pat in enumerate(os.listdir(path)):
+        print("{}) {}".format(idx, pat))
+    i = input("Enter Number: ")
+    try:
+        if 0 <= int(i) <= len(os.listdir(path)):
+            dataPath = os.listdir(path)[int(i)]
+            print("Path to Data Stored: {}". format(path + "\\" + dataPath))
+            return path + "\\" + dataPath
+    except:
+        print("Invalid Selection")
+	return None
     
